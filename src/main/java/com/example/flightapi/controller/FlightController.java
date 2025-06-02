61992c47846a0e94e4e1c1f3513cf495aac0be14
Han-Xu-hx <hanxu@cn.ibm.com>
2025-06-02 15:50:09 +0800

    Change the back-end logics.

--- src/main/java/com/example/flightapi/controller/FlightController.java
+++ src/main/java/com/example/flightapi/controller/FlightController.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/example/flightapi/services/FlightService.java
+++ src/main/java/com/example/flightapi/services/FlightService.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
